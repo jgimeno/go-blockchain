e415f7ffba9ec6ded61428c949c2f412a0e47a1f
Jonathan Gimeno <jgimeno@gmail.com>
2018-02-09 23:52:15 +0100

    End creating proof of work.

---
+++ pow/pow.go
@@ -0,0 +1,70 @@
+package pow
+
+import (
+	"github.com/jgimeno/go-blockchain/block"
+	"math/big"
+	"math"
+	"bytes"
+	"encoding/binary"
+	"crypto/sha256"
+)
+
+const targetBits = 2 * 8
+const maxNonce = math.MaxInt64
+
+type ProofOfWork struct {
+	block *block.Block
+	target *big.Int
+}
+
+func New(b *block.Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256 - targetBits))
+
+	return &ProofOfWork{
+		target: target,
+		block: b,
+	}
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	nonce := 0
+	var hashInt big.Int
+	var hash [32]byte
+
+	for nonce < maxNonce {
+		data := pow.codifyBlock(nonce)
+
+		hash = sha256.Sum256(data)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+
+	return nonce, hash[:]
+}
+
+func (pow *ProofOfWork) codifyBlock(nonce int) []byte {
+	nonceInt64 := uint64(nonce)
+	var targetBits uint64 = targetBits
+
+	nonceByteSlice := make([]byte, 8)
+	binary.LittleEndian.PutUint64(nonceByteSlice, nonceInt64)
+	targetBitsByteSlice := make([]byte, 8)
+	binary.LittleEndian.PutUint64(targetBitsByteSlice, targetBits)
+
+	data := bytes.Join(
+		[][]byte{
+		pow.block.PrevBlockHash,
+		pow.block.Data,
+		nonceByteSlice,
+		},
+		[]byte{},
+	)
+
+	return data
+}
---
+++ pow/pow_test.go
@@ -0,0 +1,23 @@
+package pow
+
+import (
+	"testing"
+	"github.com/jgimeno/go-blockchain/block"
+)
+
+func TestUsageOfProofOfWork(t *testing.T) {
+	b := block.New([]byte("The data"), []byte("PrevHash"))
+	pow := New(b)
+
+	_, h := pow.Run()
+
+	numBytes := 2
+
+	for i := 0; i < numBytes; i++ {
+		if h[i] != 0 {
+			t.Fatalf("Failed asserting that byte num %v is 0, %v instead.")
+		}
+	}
+}
+
+
